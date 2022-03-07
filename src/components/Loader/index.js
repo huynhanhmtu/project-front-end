e7ce957d21ad3fa64ebcf6b783a6ba612ebbe864
Duy Anh <huynhanh.mtu@gmail.com>
2022-03-07 23:33:03 +0700

    Base structure

---
+++ src/components/Loader/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+const Loader = styled.div`
+  margin: auto;
+  margin-top: 1rem;
+  border: 4px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 4px solid ${(props) => (props.color ? "blue" : "#3498db")};
+width: 30px;
+height: 30px;
+animation: ${spin} 1s linear infinite;
+`
+const Notification = styled.p`
+margin-top: 1rem;
+text-align: center;
+`
+
+export default function Loading() {
+  return (
+    <div>
+      <Loader />
+      <Notification>
+        Loading...
+      </Notification>
+    </div>
+  )
+}
