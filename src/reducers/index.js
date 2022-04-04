7b906a38622cf584bca9ee4e0e667132cb7f5ece
Duy Anh <huynhanh.mtu@gmail.com>
2022-04-04 16:01:00 +0700

    Fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,7 @@ import searchingReducer from "containers/HomeTemplate/HomePage/modules/reducer";
 import subJobsReducer from "containers/HomeTemplate/SubJobsPage/modules/reducer";
 import jobDetailReducer from "containers/HomeTemplate/JobDetailsPage/modules/reducer";
 import userLoginReducer from "containers/AdminTemplate/LoginPage/modules/reducer";
-import signUpReducer from "containers/AdminTemplate/SignupPage/modules/reducer";
+import signUpReducer from "containers/AdminTemplate/SignUpPage/modules/reducer";
 import modalReducer from "containers/AdminTemplate/_components/Modal/modules/reducer";
 import usersManagementReducer from "containers/AdminTemplate/UsersManagementPage/modules/reducer";
 
